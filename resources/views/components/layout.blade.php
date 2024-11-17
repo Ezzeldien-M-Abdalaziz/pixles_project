eeb32b63546bc5ab5ee7c2ef6d573abd6ada958d
Ezzeldien Magdy <ezzeldien.m.abdalziz@gmail.com>
2024-11-17 23:39:45 +0200

    making static pages

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -5,23 +5,39 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Pixel Positions</title>
-    @vite(['resources/css/app.css'])
+    @vite(['resources/js/app.js' , 'resources/css/app.css'])
 </head>
-<body>
-    <div>
-        <nav>
-            <a href="">
-                <img src="{{Vite::asset('resources/images/logo.svg')}}" alt="">
-            </a>
-            <div>links</div>
-            <div>post a job</div>
-            <div style="font-size: 50px">
+
+<body class="bg-black text-white">
+
+    <div class="px-10">
+
+        <nav class="flex justify-between items-center py-4 border-b border-white/10">
+
+            <div>
+                <a href="">
+                    <img src="{{Vite::asset('resources/images/logo.svg')}}" alt="">
+                </a>
+            </div>
+
+            <div class="space-x-6 font-bold">
+                <a href="#">Jops</a>
+                <a href="#">Careers</a>
+                <a href="#">Salaries</a>
+                <a href="#">Companies</a>
+            </div>
 
 
-        </div>
+            <div>
+                <a href="">Post a Job</a>
+            </div>
+
         </nav>
 
-        <main>
+
+
+
+        <main class="mt-10 max-w-[986px] mx-auto">
             {{$slot}}
         </main>
 
